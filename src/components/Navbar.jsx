eed3de5a0e93453e041d7a521c47435752589a39
Vikrant Gajria <vikrantgajria@gmail.com>
2019-01-14 15:55:41 +0530

    Add buddy component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
+import AnimatedInput from '../buddy-component/input-animated';
 
 function Navbar(props) {
     return (
         <form class="grid-flex">
             <div class="column w-50">
-                <div class="input-animaton">
-                    <label for="first-name">First Name</label>
-                    <input type="text" id="first-name" name="first-name" required />
-                </div>
+                <AnimatedInput label="First Name" type="text" id="first-name" name="first-name" required />
             </div>
             <div class="column w-50">
-                <div class="input-animation">
+                <div class="input-animated">
                     <label for="last-name">Last name</label>
                     <input type="text" id="last-name" name="last-name" required />
                 </div>
